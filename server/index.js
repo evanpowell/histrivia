3ecafe9a49ca2c3f504bc1291b59610573821ec5
Evan Powell <evanpowellmusic@gmail.com>
2018-04-06 13:57:09 -0500

    try again

--- server/index.js
+++ server/index.js
@@ -6,9 +6,9 @@ let app = express();
 
 app.use(express.static(__dirname, '../client/dist'));
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World');
-// });
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 
 
 app.listen(process.env.PORT, () => console.log(`Listening on ${process.env.PORT}`));
