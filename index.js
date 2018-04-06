595a454358a122bbca1c109c4818c6db13f2c8f2
Evan Powell <evanpowellmusic@gmail.com>
2018-04-06 12:19:46 -0500

    Serve get request

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ const express = require('express');
 
 let app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 app.listen(process.env.PORT, () => console.log(`Listening on ${process.env.PORT}`));
