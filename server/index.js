bb619f8130f7293bc9738769905fb15e9cb3d9aa
Evan Powell <evanpowellmusic@gmail.com>
2018-04-06 13:44:41 -0500

    Set up react project structure

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const db = require('../database/index');
 
 
 let app = express();
 
-app.get('/', (req, res) => {
+app.use(express.static(__dirname, '../client/dist'))
+
+.get('/', (req, res) => {
   res.send('Hello World');
-});
+})
+
+
 app.listen(process.env.PORT, () => console.log(`Listening on ${process.env.PORT}`));
