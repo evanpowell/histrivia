bbfc24017a87db663536bd7387277aff5375bed1
Evan Powell <evanpowellmusic@gmail.com>
2018-04-09 00:35:32 -0500

    hmm

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.post('/login', (req, res) => {
     .select('score')
     .then(result => {
       console.log(result);
-      res.send(JSON.stringify(result));
+      res.send(result[0].score);
     })
     .catch(err => {
       console.log(err);
